ac32fef17d73db2407d90eedb885d1d87bc20506
Thomas Bolinger <bolitj01@pfw.edu>
2025-12-03 14:02:58 -0500

    Added test solution and script

--- cypress/e2e/animalsearch.cy.js
+++ cypress/e2e/animalsearch.cy.js
@@ -4,10 +4,18 @@ describe("Tag Search", () => {
   });
 
   it("Clears the database", () => {
-    //TODO
+    cy.get('[data-testid="clear"]').click();
+    cy.get('[data-testid="result"]').should("contain", "All animals deleted.");
   });
 
   it("Uploads an animal searches for it", () => {
-    //TODO
+    cy.get('[data-testid="animal"]').type("Lion");
+    cy.get('[data-testid="url"]').type("https://i1.sndcdn.com/avatars-zlqzZpaIOogVFZ5a-jjNVHw-t500x500.jpg");
+    cy.get('[data-testid="upload"]').click();
+    cy.get('[data-testid="result"]').should("contain", "Animal Lion uploaded!");
+    cy.get('[data-testid="query"]').type("Lion");
+    cy.get('[data-testid="search"]').click();
+    //Check image src is correct
+    cy.get('[data-testid="image"]').should("have.attr", "src", "https://i1.sndcdn.com/avatars-zlqzZpaIOogVFZ5a-jjNVHw-t500x500.jpg");
   });
 });
