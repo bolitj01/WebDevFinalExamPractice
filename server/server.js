8d96521b91a5aa37b934c143e5bf9dc15088a8ae
Thomas Bolinger <bolitj01@pfw.edu>
2025-12-03 01:56:48 -0500

    Easier server template variables and DB connection URL

--- server/server.js
+++ server/server.js
@@ -6,9 +6,20 @@ import url from 'url';
 
 const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
 const buildFolder = "../dist";
+const buildPath = path.join(__dirname, buildFolder);
+const indexPath = path.join(buildPath, "index.html");
 
-//TODO
+const dbURL = "mongodb+srv://final-exam-practice:final-exam-practice@pfw-cs.ctovaum.mongodb.net/final-exam-practice?retryWrites=true&w=majority";
 
-//TESTING:
+const app = express();
+
+//TODO Implement missing structures and functionality
+//DB connection, middleware, and API routes
+
+const port = 8080;
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
+
 //npm run dev to start React app and Express server
 //npm run prod to build React app and start Express server
